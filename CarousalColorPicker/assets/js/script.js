f04a70475811aac3aeea0698227102cfc6896995
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-05-24 12:54:02 +0530

    FEAT. Color Picker and Background Change Done

--- CarousalColorPicker/assets/js/script.js
+++ CarousalColorPicker/assets/js/script.js
@@ -1,1 +1,8 @@
-// This is file structure for script.js
+const colorPicker = document.getElementById('colorPicker');
+
+colorPicker.addEventListener('input', function () {
+  const selectedColor = colorPicker.value;
+  console.log(selectedColor);
+  document.getElementById('wholeBody').style.backgroundColor = selectedColor;
+  document.getElementById('displayColor').innerText = 'Color Picked: ' + selectedColor;
+});
