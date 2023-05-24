db90ab71e7b2af302e28ce192a9352db3862e567
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-05-24 14:06:38 +0530

    FEAT. Preserving color on slide Done

--- CarousalColorPicker/assets/js/script.js
+++ CarousalColorPicker/assets/js/script.js
@@ -6,26 +6,40 @@ colorPicker.addEventListener('input', function () {
   document.getElementById('wholeBody').style.backgroundColor = selectedColor;
   document.getElementById('displayColor').innerText = 'Color Picked: ' + selectedColor;
 });
+
 const totalPages = 4;
 if(localStorage.getItem("currentPage")==null){
     console.log("eneteered ifff");
+    localStorage.setItem("1",'white');
+    localStorage.setItem("2",'white');
+    localStorage.setItem("3",'white');
     localStorage.setItem("currentPage",1);
 }
 
-
+var currentPage=localStorage.getItem("currentPage");
+var color=localStorage.getItem(currentPage);
+document.getElementById("wholeBody").style.backgroundColor=color;
+document.getElementById("displayColor").innerText='Color Picked: '+color;
 function nextPage() {
     console.log("**"+localStorage.getItem("currentPage"));
+    var element =document.getElementById("wholeBody");
+    var backgroundColor = window.getComputedStyle(element).backgroundColor;
    var currentPage=localStorage.getItem("currentPage");
+   localStorage.setItem(currentPage,backgroundColor);
   if (currentPage < totalPages) {
     currentPage++;
     localStorage.setItem("currentPage",currentPage);
+   
     loadPage();
   }
 }
 
 function prevPage() {
  console.log("**"+localStorage.getItem("currentPage"));
- var currentPage=localStorage.getItem("currentPage");
+ var element =document.getElementById("wholeBody");
+ var backgroundColor = window.getComputedStyle(element).backgroundColor;
+   var currentPage=localStorage.getItem("currentPage");
+   localStorage.setItem(currentPage,backgroundColor);
   if (currentPage > 1) {
     currentPage--;
     localStorage.setItem("currentPage",currentPage)
